df2e29e0a1105efa3db787313821eb2f8e419082
G0DiegoG0 <116124417+G0DiegoG0@users.noreply.github.com>
2023-05-02 15:23:33 -0700

    finished
    turned it in

--- RomoUnit5/Assets/Course Library/_Source_Files/Scripts/GameManager.cs
+++ RomoUnit5/Assets/Course Library/_Source_Files/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,12 +12,13 @@ public class GameManager : MonoBehaviour
     private int score;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
+    public GameObject titleScreen;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
+      
     }
 
     // Update is called once per frame
@@ -26,12 +29,11 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-       while (true)
+       while (isGameActive)
        {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
-            UpdateScore(5);
        }
     }
 
@@ -43,6 +45,24 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
+        restartButton.gameObject.SetActive(true);
         gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        spawnRate /= difficulty;
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+
     }
 }
